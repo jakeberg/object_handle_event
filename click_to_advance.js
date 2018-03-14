a496ffe6a2554d0a9fbcef9f0b927d58c90d581f
Jake Berg <jacobvictorberg@gmail.com>
2018-03-14 12:08:02 -0400

    finished assignment

--- click_to_advance.js
+++ click_to_advance.js
@@ -1,19 +1,25 @@
 const flower_images = ["flower0.png", "flower1.png", "flower2.png"];
 const container_div = document.getElementById("container_div");
 
+
 /* frame_images is an array of image urls.
  * target_div is the HTML element within which to append the image.
  * x, y are the coordinates for where to position the image.
  */
 function ClickToAdvance(frame_images, target_div, x, y) {
     this.images = frame_images; // An array of image urls
-    this.frame = 0;             // Index of the frame to show
+    this.frame = 0; // Index of the frame to show
     this.img = document.createElement("img");
     this.img.src = this.images[0];
     this.img.style.position = "absolute";
     this.img.style.left = x + "px";
     this.img.style.top = y + "px";
     target_div.appendChild(this.img);
+
+    this.handleEvent = function(){
+        this.frame++;
+        this.img.src = this.images[this.frame];
+    }
 
     /* TODO: implement a method named handleEvent
      * handleEvent should advance to the next frame in the sequence.
@@ -27,12 +33,13 @@ function ClickToAdvance(frame_images, target_div, x, y) {
      * you can uncomment the line below to add this object as the event listener
      * for clicks on the image.
      */
-     
-     // this.img.addEventListener("click", this);
+
+    this.img.addEventListener("click", this);
 }
 
-for(var i = 0; i < 5; i++) {
+for (var i = 0; i < 5; i++) {
     var x = 150 * i;
     var y = Math.floor(Math.random() * 500);
     var flower = new ClickToAdvance(flower_images, container_div, x, y);
 }
+
